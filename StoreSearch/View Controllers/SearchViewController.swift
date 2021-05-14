878fc33117972f17383d09c3d2e0be31da7ec911
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-15 01:05:25 +0300

    Add searchBar and tableView outlets and connect them to view controller. Add Search Bar Delegate and Table View Delegate Methods. Define SearchViewController as tableView, searchBar delegate and tableView data source.

--- StoreSearch/View Controllers/SearchViewController.swift
+++ StoreSearch/View Controllers/SearchViewController.swift
@@ -8,9 +8,40 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.contentInset = UIEdgeInsets(top: 56, left: 0, bottom: 0, right: 0)
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
 
+// MARK: - Search Bar Delegate
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is: '\(searchBar.text!)'")
+    }
+}
+
+// MARK: - Table View Delegate
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 0
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
