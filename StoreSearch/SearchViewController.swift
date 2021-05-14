f7274d1b37cebd98e7f78d27d89de7c6c2c72837
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-14 22:56:05 +0300

    Create project named StoreSearch.

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Tolga PIRTURK on 14.05.2021.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
