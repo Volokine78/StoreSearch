d0cd00eb76cacbecbca7cf4fda862d518fe17ead
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-15 03:17:50 +0300

    Add Data Model folder with SearchResult file. Add name and artistName properties.

---
+++ StoreSearch/Data Model/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Tolga PIRTURK on 15.05.2021.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
