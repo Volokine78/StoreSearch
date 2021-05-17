8c8402f83a5ceccba9c8a08a87fbc4af815b8c15
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-18 01:33:58 +0300

    Create Table View folder on StoreSearch project. Add SearchResultCell file to Table View folder.

---
+++ StoreSearch/Table View/SearchResultCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Tolga PIRTURK on 18.05.2021.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
