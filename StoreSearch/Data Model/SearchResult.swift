ba065ef12ce4ba29c4282bdbaae1c3d9add1f845
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-22 04:12:29 +0300

    Make SearchResult object conform CustomStringConvertible with description.

--- StoreSearch/Data Model/SearchResult.swift
+++ StoreSearch/Data Model/SearchResult.swift
@@ -12,7 +12,11 @@ class ResultArray: Codable {
     var results = [SearchResult]()
 }
 
-class SearchResult: Codable {
+class SearchResult: Codable, CustomStringConvertible {
+    var description: String {
+        return "\nResult - Name: \(name), Artist Name: \(artistName ?? "None")"
+    }
+    
     var artistName: String? = ""
     var trackName: String? = ""
     
