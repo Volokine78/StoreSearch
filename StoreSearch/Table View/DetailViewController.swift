7dbd0cdb4b6b3acd544eab4e794f35387a1f8027
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-27 18:06:11 +0300

    Add an extension to DetailViewController to make it UIGestureRecognizerDelegate. Make gesture recognizer listens to taps anywhere in this view controller and calls the close() method in response.

--- StoreSearch/Table View/DetailViewController.swift
+++ StoreSearch/Table View/DetailViewController.swift
@@ -20,21 +20,25 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         popupView.layer.cornerRadius = 10
+        
+        let gestureRecognizer = UITapGestureRecognizer(
+            target: self, action: #selector(close))
+        gestureRecognizer.cancelsTouchesInView = false
+        gestureRecognizer.delegate = self
+        view.addGestureRecognizer(gestureRecognizer)
     }
     
     // MARK: - Actions
     @IBAction func close() {
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension DetailViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldReceive touch: UITouch
+    ) -> Bool {
+        return (touch.view === self.view)
     }
-    */
-
 }
