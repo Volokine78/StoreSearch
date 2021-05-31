bc1f365aa7f5c48bd5c856623ec02f9679314eb7
Tolga Pirtürk <tpirturk@gmail.com>
2021-06-01 00:40:00 +0300

    Add firstTime private variable to call tileButtons(_:) method only once. Add tileButtons(_:) method to perform the math and place the buttons on the screen in rows and columns.

--- StoreSearch/View Controllers/LandscapeViewController.swift
+++ StoreSearch/View Controllers/LandscapeViewController.swift
@@ -13,6 +13,7 @@ class LandscapeViewController: UIViewController {
     @IBOutlet var pageControl: UIPageControl!
     
     var searchResults = [SearchResult]()
+    private var firstTime = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,5 +40,66 @@ class LandscapeViewController: UIViewController {
             y: safeFrame.size.height - pageControl.frame.size.height,
             width: safeFrame.size.width,
             height: pageControl.frame.size.height)
+        
+        if firstTime {
+            firstTime = false
+            tileButtons(searchResults)
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func tileButtons(_ searchResults: [SearchResult]) {
+        let itemWidth: CGFloat = 94
+        let itemHeight: CGFloat = 88
+        var columnsPerPage = 0
+        var rowsPerPage = 0
+        var marginX: CGFloat = 0
+        var marginY: CGFloat = 0
+        let viewWidth = scrollView.bounds.size.width
+        let viewHeight = scrollView.bounds.size.height
+        
+        columnsPerPage = Int(viewWidth / itemWidth)
+        rowsPerPage = Int(viewHeight / itemHeight)
+        
+        marginX = (viewWidth - (CGFloat(columnsPerPage) * itemWidth)) * 0.5
+        marginY = (viewHeight - (CGFloat(rowsPerPage) * itemHeight)) * 0.5
+        
+        let buttonWidth: CGFloat = 82
+        let buttonHeight: CGFloat = 82
+        let paddingHorz = (itemWidth - buttonWidth) / 2
+        let paddingVert = (itemHeight - buttonHeight) / 2
+        
+        var row = 0
+        var column = 0
+        var x = marginX
+        for (index, _) in searchResults.enumerated() {
+            let button = UIButton(type: .system)
+            button.backgroundColor = UIColor.white
+            button.setTitle("\(index)", for: .normal)
+            
+            button.frame = CGRect(
+                x: x + paddingHorz,
+                y: marginY + CGFloat(row) * itemHeight + paddingVert,
+                width: buttonWidth,
+                height: buttonHeight)
+            
+            scrollView.addSubview(button)
+            
+            row += 1
+            if row == rowsPerPage {
+                row = 0; x += itemWidth; column += 1
+                if column == columnsPerPage {
+                    column = 0; x += marginX * 2
+                }
+            }
+        }
+        
+        let buttonsPerPage = columnsPerPage * rowsPerPage
+        let numPages = 1 + (searchResults.count - 1) / buttonsPerPage
+        scrollView.contentSize = CGSize(
+            width: CGFloat(numPages) * viewWidth,
+            height: scrollView.bounds.size.height)
+        
+        print("Number of pages: \(numPages)")
     }
 }
