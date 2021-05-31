4364a7a7cd9e46d1afdb2b83ec5069b0899e159a
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-31 18:53:45 +0300

    Add scrollView and pageControl outlet variables. Disable auto layout for the view controller.

--- StoreSearch/View Controllers/LandscapeViewController.swift
+++ StoreSearch/View Controllers/LandscapeViewController.swift
@@ -8,8 +8,20 @@
 import UIKit
 
 class LandscapeViewController: UIViewController {
+    
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var pageControl: UIPageControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
     }
 }
