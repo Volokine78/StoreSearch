402debf0ae0ffc91547c3a9e8914c8e6417757e9
Tolga Pirtürk <tpirturk@gmail.com>
2021-05-31 03:16:24 +0300

    Add animation to showLandscape(with:) and hideLandscape(with:) methods.

--- StoreSearch/View Controllers/SearchViewController.swift
+++ StoreSearch/View Controllers/SearchViewController.swift
@@ -115,20 +115,33 @@ class SearchViewController: UIViewController {
         
         landscapeVC = storyboard!.instantiateViewController(
             withIdentifier: "LandscapeViewController") as? LandscapeViewController
+        
         if let controller = landscapeVC {
             controller.view.frame = view.bounds
+            controller.view.alpha = 0
+            
             view.addSubview(controller.view)
             addChild(controller)
-            controller.didMove(toParent: self)
+            
+            coordinator.animate(alongsideTransition: { _ in
+                controller.view.alpha = 1
+            } , completion: { _ in
+                controller.didMove(toParent: self)
+            })
         }
     }
     
     func hideLandscape(with coordinator: UIViewControllerTransitionCoordinator) {
         if let controller = landscapeVC {
             controller.willMove(toParent: nil)
-            controller.view.removeFromSuperview()
-            controller.removeFromParent()
-            landscapeVC = nil
+            
+            coordinator.animate(alongsideTransition: { _ in
+                controller.view.alpha = 0
+            } , completion: { _ in
+                controller.view.removeFromSuperview()
+                controller.removeFromParent()
+                self.landscapeVC = nil
+            })
         }
     }
 }
