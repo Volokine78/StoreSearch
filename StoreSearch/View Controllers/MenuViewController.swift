70459330d7761eb51468851e1526078a10d499be
Tolga Pirtürk <tpirturk@gmail.com>
2021-06-19 22:17:32 +0300

    Add MenuViewController and set it as popover table view controller's class.

---
+++ StoreSearch/View Controllers/MenuViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuViewController.swift
+//  StoreSearch
+//
+//  Created by Tolga PIRTURK on 19.06.2021.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
