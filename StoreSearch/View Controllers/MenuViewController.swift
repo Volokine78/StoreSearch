ad3c14a8f9a7886cccd5676f1e2e67c44f6de56a
Tolga Pirtürk <tpirturk@gmail.com>
2021-06-19 22:26:37 +0300

    Add MenuViewControllerDelegate protocol. Add tableView(_:didSelectRowAt:) delegate method.

--- StoreSearch/View Controllers/MenuViewController.swift
+++ StoreSearch/View Controllers/MenuViewController.swift
@@ -8,9 +8,26 @@
 import UIKit
 
 class MenuViewController: UITableViewController {
-
+    
+    weak var delegate: MenuViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            delegate?.menuViewControllerSendEmail(self)
+        }
+    }
+}
+
+protocol MenuViewControllerDelegate: AnyObject {
+    func menuViewControllerSendEmail(_ controller: MenuViewController)
 }
